704d4969915a04ba42487a40c2eaee6935de20d3
Jordan Schneidler <jordan.schneidler@gmail.com>
2021-10-22 19:17:33 -0600

    fs_utils refactoring and tests.

--- compiler/jack_compiler/fs_utils.py
+++ compiler/jack_compiler/fs_utils.py
@@ -1,21 +1,25 @@
 from pathlib import Path
 
 
-def validateInputPath(inputPath: Path):
+def validateInputPath(inputPath: Path) -> None:
+    """Validates the input path, currently just checks the path exists."""
     if not inputPath.exists():
-        raise Exception('Invalid input path: ' + str(inputPath))
+        raise Exception("Invalid input path: " + str(inputPath))
 
 
-def generateOutputPath(inputPath: Path):
-    return inputPath.stem + '.xml'
+def generateOutputPath(inputPath: Path) -> Path:
+    """Swap the extension of the file/folder name with .xml"""
+    return Path(inputPath.stem + ".xml")
 
 
-def isJackFile(path: Path):
-    return path.suffix == '.jack'
+def isJackFile(path: Path) -> bool:
+    """Does the file end in .jack?"""
+    return path.suffix == ".jack"
 
 
-def getJackFilesInDirectory(directory: Path):
-    jackFiles = []
+def getJackFilesInDirectory(directory: Path) -> list[Path]:
+    """Returns a list of .jack files in directory"""
+    jackFiles: list[Path] = []
     for file in directory.iterdir():
         if isJackFile(file):
             jackFiles.append(file)
--- compiler/tests/test_fs_utils.py
+++ compiler/tests/test_fs_utils.py
@@ -1,2 +1,73 @@
-def test_pytest():
-    assert 2 == 2
+import pytest
+from pathlib import Path
+from jack_compiler import fs_utils
+
+
+def test_validateInputPath():
+    # Success scenarios
+    fs_utils.validateInputPath(Path("tests/jackFiles"))
+    fs_utils.validateInputPath(Path("tests/jackFiles/"))
+    fs_utils.validateInputPath(Path("tests/jackFiles/Main.jack"))
+    fs_utils.validateInputPath(Path("./tests/jackFiles"))
+    fs_utils.validateInputPath(Path("./tests/jackFiles/"))
+    fs_utils.validateInputPath(Path("./tests/jackFiles/Main.jack"))
+
+    # Failure scenarios
+    with pytest.raises(Exception):
+        fs_utils.validateInputPath("invalid")
+        fs_utils.validateInputPath("./invalid")
+        fs_utils.validateInputPath("tests/invalid")
+        fs_utils.validateInputPath("./tests/invalid")
+
+
+def test_generateOutputPath():
+    path = Path("Main.jack")
+    outputPath = fs_utils.generateOutputPath(path)
+    assert outputPath.name == "Main.xml"
+
+    path = Path("./Main.jack")
+    outputPath = fs_utils.generateOutputPath(path)
+    assert outputPath.name == "Main.xml"
+
+    path = Path("/Main.jack")
+    outputPath = fs_utils.generateOutputPath(path)
+    assert outputPath.name == "Main.xml"
+
+    path = Path("tests/Main.jack")
+    outputPath = fs_utils.generateOutputPath(path)
+    assert outputPath.name == "Main.xml"
+
+    path = Path("tests/Main.jack")
+    outputPath = fs_utils.generateOutputPath(path)
+    assert outputPath.name == "Main.xml"
+
+    path = Path("tests/jackFiles")
+    outputPath = fs_utils.generateOutputPath(path)
+    assert outputPath.name == "jackFiles.xml"
+
+    path = Path("tests/jackFiles/")
+    outputPath = fs_utils.generateOutputPath(path)
+    assert outputPath.name == "jackFiles.xml"
+
+
+def test_isJackFile():
+    assert fs_utils.isJackFile(Path("Main.jack")) is True
+    assert fs_utils.isJackFile(Path("./Main.jack")) is True
+    assert fs_utils.isJackFile(Path("/Main.jack")) is True
+    assert fs_utils.isJackFile(Path("tests")) is False
+    assert fs_utils.isJackFile(Path("./tests")) is False
+    assert fs_utils.isJackFile(Path("/tests")) is False
+    assert fs_utils.isJackFile(Path("tests/Main.jack")) is True
+    assert fs_utils.isJackFile(Path("./tests/Main.jack")) is True
+    assert fs_utils.isJackFile(Path("/tests/Main.jack")) is True
+    assert fs_utils.isJackFile(Path("tests/Main.abc")) is False
+    assert fs_utils.isJackFile(Path("./tests/Main.abc")) is False
+    assert fs_utils.isJackFile(Path("/tests/Main.abc")) is False
+
+
+def test_getJackFilesInDirectory():
+    files = fs_utils.getJackFilesInDirectory(Path("tests/jackFiles"))
+    assert files == [Path("tests/jackFiles/Main.jack")]
+
+    files = fs_utils.getJackFilesInDirectory(Path("tests"))
+    assert files == []
